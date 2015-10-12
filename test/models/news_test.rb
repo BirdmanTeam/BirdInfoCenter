33980b4b879f90ee9cc0e01e04aff8138b2785c3
Andy Hobbs <andyhobbs@gmail.com>
2015-10-12 16:32:55 +0300

    tests for news model

--- test/models/news_test.rb
+++ test/models/news_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
-class NewsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Page do
+  it { expect(build(:news)).to be_valid }
+  it { should validate_length_of(:name).is_at_least(2) }
+  it { should validate_length_of(:description).is_at_least(2) }
+  it { should validate_length_of(:branch).is_at_least(2) }
+  it { should validate_length_of(:popular).is_at_least(2) }
+  it { should validate_length_of(:date).is_at_least(2) }
+  it { should validate_length_of(:created_at).is_at_least(2) }
+  it { should validate_presence_of(:updated_at) }
 end
