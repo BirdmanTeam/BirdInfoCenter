f6a9f60d6b6fb8d6713d7f806279c4a70b44b190
Taras Shpachenko <taras.shpachenko@yandex.ru>
2015-10-13 12:52:25 +0300

    Music finish parse first five pages

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -17,25 +17,33 @@ class NewsController < ApplicationController
   private
 
   def update_music
-    url = open('http://www.rollingstone.com/music/news')
+    count_of_pages = 5
+    i = 1
+    while i<=count_of_pages
+      if i==1
+        url = open('http://www.rollingstone.com/music/news')
+      else
+        url = open('http://www.rollingstone.com/music/news?page='+i.to_s)
+      end
+      doc = Nokogiri::HTML(url)
 
-    doc = Nokogiri::HTML(url)
+      doc.css('li.primary-list-item').each do |link|
+        article_url = open('http://www.rollingstone.com'+link.at_css('.list-item-hd a')['href'])
+        article_doc = Nokogiri::HTML(article_url)
 
-    doc.css('li.primary-list-item').each do |link|
-      article_url = open('http://www.rollingstone.com'+link.at_css('.list-item-hd a')['href'])
-      article_doc = Nokogiri::HTML(article_url)
+        @title = article_doc.css('.article-title').text
+        @sub_title = article_doc.css('.article-sub-title').text
 
-      @title = article_doc.css('.article-title').text
-      @sub_title = article_doc.css('.article-sub-title').text
+        date_unformat = link.css('span.datestamp').text
+        month = Date::MONTHNAMES.index(date_unformat.split(' ')[0])
+        day = (date_unformat.split(' ')[1][0..-2].to_i+1).to_s
+        year = date_unformat.split(' ')[2]
+        @date = ("#{day}/#{month}/#{year}").to_time
 
-      date_unformat = link.css('span.datestamp').text
-      month = Date::MONTHNAMES.index(date_unformat.split(' ')[0])
-      day = (date_unformat.split(' ')[1][0..-2].to_i+1).to_s
-      year = date_unformat.split(' ')[2]
-      @date = ("#{day}/#{month}/#{year}").to_time
-
-      @content = article_doc.css('.article-content p').text
-      News.create(name: @title, description: @content, branch: 'Music', date: @date)
+        @content = article_doc.css('.article-content p').text
+        News.create(name: @title, description: @content, branch: 'Music', date: @date)
+      end
+      i+=1
     end
   end
 
