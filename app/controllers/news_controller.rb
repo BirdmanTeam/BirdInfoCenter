6f4ad131f013668dcf783230763ee3fb6ad93821
Alexandr Orlov <morozhunce@gmail.com>
2015-11-11 20:30:48 +0200

    catch exception on fields not present + adding news as arrays

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -9,10 +9,8 @@ class NewsController < ApplicationController
   def parse_news
     News.delete_all
 
-    update_music(1)
-    update_economic(1)
+
     update_politic(1)
-    update_sport(1)
 
     redirect_to root_path
   end
@@ -26,40 +24,47 @@ class NewsController < ApplicationController
   private
 
   def update_music(count_of_pages)
+    news_array = Array.new
     count_of_pages.times do |i|
       url = open('http://www.rollingstone.com/music/news?page=' + (i + 1).to_s)
 
       doc = Nokogiri::HTML(url)
 
       doc.css('li.primary-list-item').each do |link|
-        article_url = open('http://www.rollingstone.com' + link.at_css('.list-item-hd a')['href'])
-        article_doc = Nokogiri::HTML(article_url)
+        begin
+          article_url = open('http://www.rollingstone.com' + link.at_css('.list-item-hd a')['href'])
+          article_doc = Nokogiri::HTML(article_url)
 
-        if article_doc.at_css('.article-img-holder img')
-          photo = article_doc.at_css('.article-img-holder img').attr('src')
-          video = 'none'
-        elsif article_doc.at_css('.article-img-holder iframe')
-          video = article_doc.at_css('.article-img-holder iframe').attr('src')
-          photo ='none'
-        else
-          photo = 'none'
-          video = 'none'
+          if article_doc.at_css('.article-img-holder img')
+            photo = article_doc.at_css('.article-img-holder img').attr('src')
+            video = 'none'
+          elsif article_doc.at_css('.article-img-holder iframe')
+            video = article_doc.at_css('.article-img-holder iframe').attr('src')
+            photo ='none'
+          else
+            photo = 'none'
+            video = 'none'
+          end
+
+          title = article_doc.css('.article-title').text
+          date_unformat = link.css('span.datestamp').text
+          month = Date::MONTHNAMES.index(date_unformat.split[0])
+          day = (date_unformat.split[1][0..-2].to_i + 1).to_s
+          year = date_unformat.split[2]
+          date = ("#{day}/#{month}/#{year}").to_time
+
+          content = article_doc.css('.article-content p').text
+          raise Exception if content.length == 0
+          news_array << { name: title, description: content, branch: 'Music', date: date, photo: photo, video: video }
+        rescue Exception
         end
-
-        title = article_doc.css('.article-title').text
-        date_unformat = link.css('span.datestamp').text
-        month = Date::MONTHNAMES.index(date_unformat.split[0])
-        day = (date_unformat.split[1][0..-2].to_i + 1).to_s
-        year = date_unformat.split[2]
-        date = ("#{day}/#{month}/#{year}").to_time
-
-        content = article_doc.css('.article-content p').text
-        News.create(name: title, description: content, branch: 'Music', date: date, photo: photo, video: video)
       end
     end
+    News.create(news_array)
   end
 
   def update_politic(count_of_pages)
+    news_array = Array.new
     count_of_pages.times do |i|
       url = open('http://www.firstpost.com/category/politics/page/' + (i + 1).to_s)
 
@@ -69,7 +74,8 @@ class NewsController < ApplicationController
       link_criteria = (i.zero?)? 'a' : 'div.FL a'
       date_criteria = (i.zero?)? '.rd_12' : '.bd12nv'
 
-        doc.css(criteria).each do |link|
+      doc.css(criteria).each do |link|
+        begin
           article_url = open(link.at_css(link_criteria)['href'])
           article_doc = Nokogiri::HTML(article_url)
 
@@ -92,75 +98,90 @@ class NewsController < ApplicationController
           date = ("#{day}/#{month}/#{year}").to_time
 
           content = article_doc.css('div.fullCont1').text
-          News.create(name: title, description: content, branch: 'Politic', date: date, photo: photo, video: video)
+          raise Exception if content.length == 0
+          news_array << { name: title, description: content, branch: 'Politic', date: date, photo: photo, video: video }
+        rescue Exception
         end
+      end
     end
+    News.create(news_array)
   end
 
   def update_economic(count_of_pages)
+    news_array = Array.new
     count_of_pages.times do |i|
       url = open('http://www.cnbc.com/economy/?page=' + (i + 1).to_s)
 
       doc = Nokogiri::HTML(url)
 
       doc.css('div.cnbcnewsstory').each do |link|
-        article_url = open('http://www.cnbc.com' + link.at_css('.headline a')['href'])
-        article_doc = Nokogiri::HTML(article_url)
+        begin
+          article_url = open('http://www.cnbc.com' + link.at_css('.headline a')['href'])
+          article_doc = Nokogiri::HTML(article_url)
 
-        if article_doc.css('.embed-container iframe')
-          #Paste iframe in video
-          video = 'none'
-          photo = 'none'
-        elsif article_doc.css('.embed-container img')
-          photo = article_doc.css('.embed-container img')[0]['src']
-          video = 'none'
-        else
-          photo = 'none'
-          video = 'none'
+          if article_doc.css('.embed-container iframe')
+            #Paste iframe in video
+            video = 'none'
+            photo = 'none'
+          elsif article_doc.css('.embed-container img')
+            photo = article_doc.css('.embed-container img')[0]['src']
+            video = 'none'
+          else
+            photo = 'none'
+            video = 'none'
+          end
+
+          title = article_doc.css('div.story-top .title').text
+          date = DateTime.parse(article_doc.at_css('div.story-top time')['datetime']).beginning_of_day
+          content = article_doc.css('#article_body p').text
+          raise Exception if content.length == 0
+          news_array << { name: title, description: content, branch: 'Economic', date: date, photo: photo, video: video }
+        rescue Exception
         end
-
-        title = article_doc.css('div.story-top .title').text
-        date = DateTime.parse(article_doc.at_css('div.story-top time')['datetime']).beginning_of_day
-        content = article_doc.css('#article_body p').text
-        News.create(name: title, description: content, branch: 'Economic', date: date, photo: photo, video: video)
       end
     end
+    News.create(news_array)
   end
 
   def update_sport(count_of_pages)
     conditions = %w(/gallery/ /picture/ /video/ /live/)
-
+    news_array = Array.new
     count_of_pages.times do |i|
       url = open('http://www.theguardian.com/sport?page=' + (i + 1).to_s)
 
       doc = Nokogiri::HTML(url)
 
       doc.css('div.fc-item__container').each do |link|
+        begin
+          link_criteria = link.at_css('div.fc-item__content a')['href']
+          next unless check_links(link_criteria, conditions)
 
-        link_criteria = link.at_css('div.fc-item__content a')['href']
-        next unless check_links(link_criteria, conditions)
+          article_url = open(link_criteria)
+          article_doc = Nokogiri::HTML(article_url)
 
-        article_url = open(link_criteria)
-        article_doc = Nokogiri::HTML(article_url)
+          if article_doc.at_css('.article__img-container img')
+            photo = article_doc.at_css('.article__img-container img').attr('src')
+            video = 'none'
+            #This code for parse video if it exist
+            # elsif article_doc.at_css('.article-img-holder iframe')
+            #   video = article_doc.at_css('.article-img-holder iframe').attr('src')
+            #   photo ='none'
+          else
+            photo = 'none'
+            video = 'none'
+          end
 
-        if article_doc.at_css('.article__img-container img')
-          photo = article_doc.at_css('.article__img-container img').attr('src')
-          video = 'none'
-          #This code for parse video if it exist
-          # elsif article_doc.at_css('.article-img-holder iframe')
-          #   video = article_doc.at_css('.article-img-holder iframe').attr('src')
-          #   photo ='none'
-        else
-          photo = 'none'
-          video = 'none'
+          title = link.css('span.js-headline-text').text
+
+          date = DateTime.parse(article_doc.at_css('div.content__meta-container .content__dateline time')['datetime']).beginning_of_day
+          content = article_doc.css('article.content div.content__main div.content__article-body p').text
+          raise Exception if content.length == 0
+          news_array << { name: title, description: content, branch: 'Sport', date: date, photo: photo, video: video }
+        rescue Exception
         end
-
-        title = link.css('span.js-headline-text').text
-        date = DateTime.parse(article_doc.at_css('div.content__meta-container .content__dateline time')['datetime']).beginning_of_day
-        content = article_doc.css('article.content div.content__main div.content__article-body p').text
-        News.create(name: title, description: content, branch: 'Sport', date: date, photo: photo, video: video)
       end
     end
+    News.create(news_array)
   end
 
   def set_news
